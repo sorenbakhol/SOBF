bbf8a910fe057965e12178bb93e848335bc60b9c
sorenbakhol <108209871+sorenbakhol@users.noreply.github.com>
2023-09-16 13:55:18 +0530

    Add files via upload

---
+++ newScript.js
@@ -0,0 +1,191 @@
+// for gulu script start from here! ***************************************************
+guluSlider.oninput = changeGuluFontSize;
+function changeGuluFontSize() {
+  guluTextArea.style.fontSize = this.value + "px";
+}
+
+guluSpace.oninput = changeGuluSpace;
+function changeGuluSpace() {
+  guluTextArea.style.letterSpacing = this.value + "px";
+}
+
+guluLine.oninput = changeGuluFontLine;
+function changeGuluFontLine() {
+  guluTextArea.style.lineHeight = this.value + "px";
+}
+
+// for usara script start from here! ***************************************************
+usaraSlider.oninput = changeUsaraFontSize;
+function changeUsaraFontSize() {
+  usaraTextArea.style.fontSize = this.value + "px";
+}
+
+usaraSpace.oninput = changeUsaraSpace;
+function changeUsaraSpace() {
+  usaraTextArea.style.letterSpacing = this.value + "px";
+}
+
+usaraLine.oninput = changeUsaraFontLine;
+function changeUsaraFontLine() {
+  usaraTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Banam script start from here! ***************************************************
+banamSlider.oninput = changeBanamFontSize;
+function changeBanamFontSize() {
+  banamTextArea.style.fontSize = this.value + "px";
+}
+
+banamSpace.oninput = changeBanamSpace;
+function changeBanamSpace() {
+  banamTextArea.style.letterSpacing = this.value + "px";
+}
+
+banamLine.oninput = changeBanamFontLine;
+function changeBanamFontLine() {
+  banamTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Pahil script start from here! ***************************************************
+pahilSlider.oninput = changePahilFontSize;
+function changePahilFontSize() {
+  pahilTextArea.style.fontSize = this.value + "px";
+}
+
+pahilSpace.oninput = changePahilFontSpace;
+function changePahilFontSpace() {
+  pahilTextArea.style.letterSpacing = this.value + "px";
+}
+
+pahilLine.oninput = changePahilFontLine;
+function changePahilFontLine() {
+  pahilTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Dahar script start from here! ***************************************************
+daharSlider.oninput = changeDaharFontSize;
+function changeDaharFontSize() {
+  daharTextArea.style.fontSize = this.value + "px";
+}
+
+daharSpace.oninput = changeDaharFontSpace;
+function changeDaharFontSpace() {
+  daharTextArea.style.letterSpacing = this.value + "px";
+}
+
+daharLine.oninput = changeDaharFontLine;
+function changeDaharFontLine() {
+  daharTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Janam script start from here! ***************************************************
+janamSlider.oninput = changeJanamFontSize;
+function changeJanamFontSize() {
+  janamTextArea.style.fontSize = this.value + "px";
+}
+
+janamSpace.oninput = changeJanamFontSpace;
+function changeJanamFontSpace() {
+  janamTextArea.style.letterSpacing = this.value + "px";
+}
+
+janamLine.oninput = changeJanamFontLine;
+function changeJanamFontLine() {
+  janamTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Kulup script start from here! ***************************************************
+kulupSlider.oninput = changeKulupFontSize;
+function changeKulupFontSize() {
+  kulupTextArea.style.fontSize = this.value + "px";
+}
+
+kulupSpace.oninput = changeKulupFontSpace;
+function changeKulupFontSpace() {
+  kulupTextArea.style.letterSpacing = this.value + "px";
+}
+
+kulupLine.oninput = changeKulupFontLine;
+function changeKulupFontLine() {
+  kulupTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Rehed script start from here! ***************************************************
+rehedSlider.oninput = changeRehedFontSize;
+function changeRehedFontSize() {
+  rehedTextArea.style.fontSize = this.value + "px";
+}
+
+rehedSpace.oninput = changeRehedFontSpace;
+function changeRehedFontSpace() {
+  rehedTextArea.style.letterSpacing = this.value + "px";
+}
+
+rehedLine.oninput = changeRehedFontLine;
+function changeRehedFontLine() {
+  rehedTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Rosa script start from here! ***************************************************
+rosaSlider.oninput = changeRosaFontSize;
+function changeRosaFontSize() {
+  rosaTextArea.style.fontSize = this.value + "px";
+}
+
+rosaSpace.oninput = changeRosaFontSpace;
+function changeRosaFontSpace() {
+  rosaTextArea.style.letterSpacing = this.value + "px";
+}
+
+rosaLine.oninput = changeRosaFontLine;
+function changeRosaFontLine() {
+  rosaTextArea.style.lineHeight = this.value + "px";
+}
+
+// for nonol script start from here! ***************************************************
+nonolSlider.oninput = changenonolFontSize;
+function changenonolFontSize() {
+  nonolTextArea.style.fontSize = this.value + "px";
+}
+
+nonolSpace.oninput = changenonolFontSpace;
+function changenonolFontSpace() {
+  nonolTextArea.style.letterSpacing = this.value + "px";
+}
+
+nonolLine.oninput = changenonolFontLine;
+function changenonolFontLine() {
+  nonolTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Nagam script start from here! ***************************************************
+nagamSlider.oninput = changeNagamFontSize;
+function changeNagamFontSize() {
+  nagamTextArea.style.fontSize = this.value + "px";
+}
+
+nagamSpace.oninput = changeNagamFontSpace;
+function changeNagamFontSpace() {
+  nagamTextArea.style.letterSpacing = this.value + "px";
+}
+
+nagamLine.oninput = changeNagamFontLine;
+function changeNagamFontLine() {
+  nagamTextArea.style.lineHeight = this.value + "px";
+}
+
+// for Men script start from here! ***************************************************
+menSlider.oninput = changeMenFontSize;
+function changeMenFontSize() {
+  menTextArea.style.fontSize = this.value + "px";
+}
+
+menSpace.oninput = changeMenFontSpace;
+function changeMenFontSpace() {
+  menTextArea.style.letterSpacing = this.value + "px";
+}
+
+menLine.oninput = changeMenFontLine;
+function changeMenFontLine() {
+  menTextArea.style.lineHeight = this.value + "px";
+}
